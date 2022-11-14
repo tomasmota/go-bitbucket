704e8cc5af83bcbe5d2a652299583c0ae8514cf6
tomas-mota <tomasrebelomota@gmail.com>
2022-11-14 19:57:12 +0100

    add repo example
    Signed-off-by: tomas-mota <tomasrebelomota@gmail.com>

---
+++ examples/repo/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/tomas-mota/go-bitbucket"
+)
+
+const (
+	projectName = "TestProject"
+	projectKey  = "TPO"
+	repoName    = "TestRepo"
+)
+
+func main() {
+	username := os.Getenv("BITBUCKET_USERNAME")
+	if username == "" {
+		log.Fatal("no username provide")
+	}
+	password := os.Getenv("BITBUCKET_PASSWORD")
+	if password == "" {
+		log.Fatal("no password provided")
+	}
+
+	c, err := bitbucket.NewClient(
+		bitbucket.Config{
+			Host:   "localhost",
+			Scheme: "http",
+
+			Username: username,
+			Password: password,
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+
+	// Create
+	_, err = c.Projects.CreateProject(ctx,
+		&bitbucket.CreateProjectRequest{
+			Name:   projectName,
+			Key:    projectKey,
+			Public: true,
+		},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r, err := c.Repos.CreateRepo(ctx,
+		&bitbucket.CreateRepoRequest{
+			ProjectKey: projectKey,
+			Name:       repoName,
+		},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Repo %s created", r.Slug)
+
+}
