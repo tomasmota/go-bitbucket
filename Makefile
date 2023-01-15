13f5886bd9b9d7bb78144027b7cca8a5896f5caa
tomasmota <tomasrebelomota@gmail.com>
2023-01-15 16:14:21 +0100

    hardcode test
    Signed-off-by: tomasmota <tomasrebelomota@gmail.com>

--- Makefile
+++ Makefile
@@ -1,10 +1,8 @@
 .PHONY: bitbucketserver
 bitbucketserver:
-	docker run --name "bitbucket" -v bitbucketVolume:/var/atlassian/application-data/bitbucket -p 80:7990 -d atlassian/bitbucket
+	podman run --name "bitbucket" -v bitbucketVolume:/var/atlassian/application-data/bitbucket -p 7990:7990 -d atlassian/bitbucket
 
-# TODO: make this run all tests without hardcoding
-# Perhaps these should just be written as actual go tests
+
 .PHONY: integration-tests
 integration-tests:
-	go run ./examples/project/main.go
-	go run ./examples/repo/main.go
+	go test ./...
--- test/integration/bitbucket.go
+++ test/integration/bitbucket.go
@@ -1,9 +1,6 @@
 package integration
 
 import (
-	"log"
-	"os"
-
 	"github.com/tomas-mota/go-bitbucket"
 )
 
@@ -11,23 +8,16 @@ var client *bitbucket.Client
 
 // Configures a client to be used in testing
 // NOTE: these integration tests require a bitbucket instance to be running on localhost
+// Username and password should be "admin"
 func init() {
-	username := os.Getenv("BITBUCKET_USERNAME")
-	if username == "" {
-		log.Fatal("no username provide")
-	}
-	password := os.Getenv("BITBUCKET_PASSWORD")
-	if password == "" {
-		log.Fatal("no password provided")
-	}
-
 	c, err := bitbucket.NewClient(
 		bitbucket.Config{
-			Host:   "localhost",
+
+			Host:   "localhost:7990",
 			Scheme: "http",
 
-			Username: username,
-			Password: password,
+			Username: "admin",
+			Password: "admin",
 		},
 	)
 	if err != nil {
